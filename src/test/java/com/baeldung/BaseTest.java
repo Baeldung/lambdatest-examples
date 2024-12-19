463e1583c1fc66301c0383b89ff36f7569ef6d71
Mohammad Faisal Khatri <mohammadfaisalkhatri@gmail.com>
2024-12-19 19:27:25 +0300

    Tests added for Selenium GitHub actions demo (#1)
    * created maven project  and gitignore file
    
    * added selenium tests

---
+++ src/test/java/com/baeldung/BaseTest.java
@@ -0,0 +1,53 @@
+package com.baeldung;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.HashMap;
+
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class BaseTest {
+
+    protected RemoteWebDriver driver;
+    protected   String          status = "failed";
+
+    @BeforeTest
+    public void setup() {
+        final String userName = System.getenv("LT_USERNAME") == null ? "LT_USERNAME" : System.getenv("LT_USERNAME");
+        final String accessKey = System.getenv("LT_ACCESS_KEY") == null ? "LT_ACCESS_KEY" : System.getenv("LT_ACCESS_KEY");
+        final String gridUrl = "@hub.lambdatest.com/wd/hub";
+        try {
+            this.driver = new RemoteWebDriver(new URL ("http://" + userName + ":" + accessKey + gridUrl), getChromeOptions());
+        } catch (final MalformedURLException e) {
+            System.out.println("Could not start the remote session on LambdaTest cloud grid");
+        }
+        this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+
+    public ChromeOptions getChromeOptions() {
+        final var browserOptions = new ChromeOptions();
+        browserOptions.setPlatformName("Windows 10");
+        browserOptions.setBrowserVersion("latest");
+        final HashMap<String, Object> ltOptions = new HashMap<String, Object>();
+        ltOptions.put("project", "LambdaTest e-commerce website automation");
+        ltOptions.put("build", "LambdaTest e-commerceV1.0.0");
+        ltOptions.put("name", "Homepage search product test");
+        ltOptions.put("w3c", true);
+        ltOptions.put("plugin", "java-testNG");
+
+        browserOptions.setCapability("LT:Options", ltOptions);
+
+        return browserOptions;
+    }
+
+    @AfterTest
+    public void tearDown() {
+
+        this.driver.executeScript("lambda-status=" + this.status);
+        this.driver.quit();
+    }
+}
---
+++ src/test/java/com/baeldung/LambdaTestEcommerceTests.java
@@ -0,0 +1,19 @@
+package com.baeldung;
+
+import com.baeldung.pages.HomePage;
+import com.baeldung.pages.SearchResultPage;
+import org.testng.annotations.Test;
+
+public class LambdaTestEcommerceTests extends BaseTest {
+    @Test
+    public void testSearchProduct () {
+
+        String productName = "iPhone";
+
+        driver.get ("https://ecommerce-playground.lambdatest.io/");
+        HomePage homePage = new HomePage (driver);
+        SearchResultPage searchResultPage = homePage.searchProduct (productName);
+        searchResultPage.verifySearchResultPageHeader (productName);
+        this.status = "passed";
+    }
+}
